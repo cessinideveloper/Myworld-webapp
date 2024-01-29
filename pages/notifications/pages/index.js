522f490ab2d3776d8925483e75773888376f5f95
Karthik ks <karthikks260703@gmail.com>
2024-01-29 21:22:08 +0530

    mmhrgl

--- pages/notifications/pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 style={{marginTop: 100}}>This is notifications page</h1>
-    </div>
-  )
-}
--- pages/profile/pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 style={{marginTop: 100}}>This is profile page</h1>
-    </div>
-  )
-}
