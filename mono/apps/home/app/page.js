6b9500c77a7a7fc6fa6d4e2b85b195d4b9953950
SOURADEEP HAZRA <V22089@students.iitmandi.ac.in>
2024-01-30 11:07:59 +0530

    added profile page and modified home page(js)

---
+++ mono/apps/home/app/page.js
@@ -0,0 +1,12 @@
+export default async function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return (
+    <div>
+      <h1>This is Homepage</h1>
+    </div>
+  );
+}
